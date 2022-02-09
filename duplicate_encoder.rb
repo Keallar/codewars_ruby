09a459acfcaa49820ebca228da8d0a8bca085b94
keallar <elysanskiy@phoenixit.ru>
2022-02-09 11:48:34 +0300

    duplicate_encoder

--- duplicate_encoder.rb
+++ duplicate_encoder.rb
@@ -2,17 +2,30 @@ require 'byebug'
 require 'rspec'
 
 def duplicate_encode(word)
-  #your code here
+  word.downcase.chars.map do |c|
+    if word.downcase.count(c) == 1
+      c = '('
+    else
+      c = ')'
+    end
+  end.join
+  # word.downcase
+  #     .chars
+  #     .map { |c| word.downcase.count(c) == 1 ? c = '(' : c = ')' }
+  #     .join
 end
 
-puts "Answer: #{duplicate_encode('din')}"
+puts "Answer1: #{duplicate_encode("din")}"
+puts "Answer2: #{duplicate_encode("recede")}"
+puts "Answer3: #{duplicate_encode("Success")}"
+puts "Answer4: #{duplicate_encode("(( @")}"
+puts "Answer5: #{duplicate_encode("All duplicated ALL DUPLICATED")}"
 
-describe "codewars" do
-  it "duplicate_encode" do
-    expect(duplicate_encode("din")).to eq("(((")
-    expect(duplicate_encode("recede")).to eq("()()()")
-    expect(duplicate_encode("Success")).to eq(")())())")
-    expect(duplicate_encode("(( @")).to eq("))((")
+describe "Fixed tests" do
+  it "should pass fixed tests" do
+    expect(duplicate_encode("din")).to eq('(((')
+    expect(duplicate_encode("recede")).to eq('()()()')
+    expect(duplicate_encode("Success")).to eq(')())())')
+    expect(duplicate_encode("(( @")).to eq('))((')
   end
 end
-
